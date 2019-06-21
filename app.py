0c8b6298fdf68bfa573fd98a0b113e3e00d7ff37
domingo <domingo.luc@orange.fr>
2019-06-21 17:14:01 +0100

    Demo

--- app.py
+++ app.py
@@ -20,12 +20,19 @@ def EntityExtraction():
     if request.method == "GET":
      return render_template('EntityExtractor.html')
     else:
-     select1 = pampo.extract_entities(request.form.get('comp_select'))
-     if select1=="Choose a text":
-         select1="Choose a text"
-     select2 = pampo.extract_entities(request.form.get('area_text'))
+     post1=0
+     select1=""
+     original1 = request.form.get('comp_select')
+     original2 = request.form.get('area_text')
+     if original1 != "Choose a text":
+      select1 = pampo.extract_entities(original1)
+      post1=1
+     if original2 != "Write your text here":
+      select2 = pampo.extract_entities(original2)
+     else:
+      select2 ="No text given"
      post_done = 1
-     return render_template('EntityExtractor.html',post_done=post_done,select1=select1,select2=select2)
+     return render_template('EntityExtractor.html',post1=post1,post_done=post_done,select1=select1,select2=select2,original2=original2,original1=original1)
 
 
 
